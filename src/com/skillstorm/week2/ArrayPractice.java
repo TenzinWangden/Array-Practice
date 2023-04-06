b578bdf13072edac597d0bc67c90f9e323af18e8
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-04-06 09:59:57 -0400

    my first project

--- src/com/skillstorm/week2/ArrayPractice.java
+++ src/com/skillstorm/week2/ArrayPractice.java
@@ -1,9 +1,12 @@
 package com.skillstorm.week2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayPractice {
 	public static void main(String[] args) {
+		String[] myFave = {"dumpling", "fried rice", "noodle"};
+		System.out.println("my 3 favirote food are "+ Arrays.toString(myFave));
 		Scanner in = new Scanner(System.in);
 		System.out.println("What are your 3 favrite food? ");
 		String[] userFood = new String[3];
