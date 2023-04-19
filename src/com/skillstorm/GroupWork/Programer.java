309c175dc404dc797b1701ce6bb2a7515f847087
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-04-19 15:42:02 -0400

    Group work

---
+++ src/com/skillstorm/GroupWork/Programer.java
@@ -0,0 +1,74 @@
+package GroupWork;
+/*
+ * Team Catchable
+   Daun
+   Blake
+   Tenzin
+ */
+import java.util.Arrays;
+
+public class Programer {
+	private static final int MAX_CAPACITY = 10;
+	private String[] language = new String[MAX_CAPACITY];
+	private int numLanguageLearned;
+	private String name;
+	private String company;
+	
+
+	public void addLanguage(String language) {
+
+		this.language[this.numLanguageLearned] = language;
+		this.numLanguageLearned ++;
+	}
+
+	public void removeLanguage(String language) {
+
+		this.language[this.numLanguageLearned] = language;
+		this.numLanguageLearned --;
+	}
+
+	public Programer() {
+	}
+
+	public Programer(String name, String company) {
+		this.name = name;
+		this.company = company;
+	}
+// Getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		// Include the name
+		result += "Name: " + this.name + "\n";
+		// Include the address
+		result += "company: " + this.company + "\n";
+		// Include a list of the flavors in stock
+		result += this.numLanguageLearned + " Language learned: \n";
+		for (int i = 0; i < this.numLanguageLearned; i++) {
+			result += this.language[i] + ", ";
+		}
+		result = result.substring(0, result.length() - 2);
+		return result;
+	}
+//	try {
+//		System.out.println(divide(1, 0));
+//	} catch (ArithmeticException e) {
+//		System.out.println("catch statement running from divide");
+//	}
+}
---
+++ src/com/skillstorm/GroupWork/ProgramerMain.java
@@ -0,0 +1,32 @@
+package GroupWork;
+/*
+ * Team Catchable
+   Daun
+   Blake
+   Tenzin
+ */
+
+public class ProgramerMain {
+	public static void main(String[] args) {
+		Programer a = new Programer();
+		
+		
+		a.addLanguage("C++");
+		a.addLanguage("Pythoin");
+		a.addLanguage("Jva");
+		a.addLanguage("C#");
+		a.addLanguage("Stack");
+		System.out.println("Test 1: " + a);
+		
+		
+//		a.addLanguage("C++");
+//		a.addLanguage("Pythoin");
+//		a.addLanguage("Jva");
+//		a.addLanguage("C#");
+//		a.addLanguage("Stack");
+//		a.addLanguage("Stack");
+//		a.removeLanguage("C++");
+		System.out.println("remove Test: " + a);
+
+	}
+}
