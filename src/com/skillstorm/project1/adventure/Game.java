02b5d7f7ecdf29b5fdafdbce610ae4e513bfc1aa
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-04-28 03:34:56 -0400

    Update 3.1: Final touch up

--- src/com/skillstorm/project1/adventure/Game.java
+++ src/com/skillstorm/project1/adventure/Game.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class Game {
 	private Player player;
 	private int currentRoom;
-	private String[] rooms;
-	private String[] monsters;
-
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.start();
