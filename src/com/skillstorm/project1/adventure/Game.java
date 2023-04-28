47c8cb0f321bf65c8806b286606ef8c694ab02ae
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-04-28 09:26:46 -0400

    Final

--- src/com/skillstorm/project1/adventure/Game.java
+++ src/com/skillstorm/project1/adventure/Game.java
@@ -1,10 +1,13 @@
 package com.skillstorm.project1.adventure;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+
 public class Game {
 	private Player player;
 	private int currentRoom;
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.start();
@@ -21,25 +24,27 @@ public class Game {
 
 		System.out.println("Welcome to the game, " + player.getName() + " the " + player.getClassName() + "!");
 		if (player.getClassName().equals("Warrior")) {
-		    System.out.println("Your stats: Strength=" + player.getStrength() + " Agility=" + player.getAgility()
-		            + "\nYou are very Strong but slow as a turtle.");
+			System.out.println("Your stats: Strength=" + player.getStrength() + " Agility=" + player.getAgility()
+					+ "\nYou are very Strong but slow as a turtle.");
 		} else {
-		    System.out.println("Your stats: Strength=" + player.getStrength() + " Agility=" + player.getAgility()
-		            + "\nYou are very fast but lack the aptitude to fight.");
+			System.out.println("Your stats: Strength=" + player.getStrength() + " Agility=" + player.getAgility()
+					+ "\nYou are very fast but lack the aptitude to fight.");
 		}
 		Scanner scanner1 = new Scanner(System.in);
 		System.out.print("\nPress Enter Key to Start the Game!");
 		String in1 = scanner.nextLine();
-		
+
 		// Initialize the rooms and monsters
-	    String[] rooms = new String[] { "living room", "kitchen", "bathroom", "bedroom", "attic", "basement", "garage", "office", "library" };
-	    String[] monsters = new String[] { "zombie", "vampire", "werewolf", "witch", "ghost", "goblin", "dragon", "demon", "skeleton" };
-	    Collections.shuffle(Arrays.asList(rooms));
-	    Collections.shuffle(Arrays.asList(monsters));
-	   
-	    int numFights = 0; // initialize fight counter
-		
-	    // Start the game loop
+		String[] rooms = new String[] { "living room", "kitchen", "bathroom", "bedroom", "attic", "basement", "garage",
+				"office", "library" };
+		String[] monsters = new String[] { "zombie", "vampire", "werewolf", "witch", "ghost", "goblin", "dragon",
+				"demon", "skeleton" };
+		Collections.shuffle(Arrays.asList(rooms));
+		Collections.shuffle(Arrays.asList(monsters));
+
+		int numFights = 0; // initialize fight counter
+
+		// Start the game loop
 		while (true) {
 			System.out.println("\nYou are currently in the " + rooms[currentRoom] + ".");
 			System.out.println("There is a " + monsters[currentRoom] + " in the room.");
@@ -52,8 +57,9 @@ public class Game {
 
 			if (player instanceof Player.Warrior) {
 				if (choice == 1) {
-					System.out.println("You fought bravely. Doue to your immense stringth, have defeated the " + monsters[currentRoom] + "! You found the next room and entered.");
-					numFights++; 
+					System.out.println("You fought bravely. Doue to your immense stringth, have defeated the "
+							+ monsters[currentRoom] + "! You found the next room and entered.");
+					numFights++;
 					currentRoom++;
 				} else {
 					System.out.println("You tried to run away but you are too slow. " + monsters[currentRoom]
@@ -63,8 +69,8 @@ public class Game {
 				}
 			} else if (player instanceof Player.Rogue) {
 				if (choice == 1) {
-					System.out.println(
-							"You bravely try to fight but having no strength " + monsters + " attecked you! You were Luckally able to avoide the monster!");
+					System.out.println("You bravely try to fight but having no strength " + monsters
+							+ " attecked you! You were Luckally able to avoide the monster!");
 					gameOver(scanner);
 					break;
 				} else {
@@ -72,12 +78,13 @@ public class Game {
 					currentRoom++;
 					numFights++;
 				}
-			}      
+			}
 			if (numFights == 3) { // check if 3 fights have been completed
-	            System.out.println("*******Congratulations, you have cleared all the monsters and reached the last room /n You have reached the exit! You are Free!*******");
-	            gameOver(scanner);
-	            break;
-	        }
+				System.out.println(
+						"*******Congratulations, you have cleared all the monsters and reached the last room /n You have reached the exit! You are Free!*******");
+				gameOver(scanner);
+				break;
+			}
 			if (currentRoom == rooms.length) {
 				System.out.println("You have won the game!");
 				gameOver(scanner);
@@ -94,6 +101,7 @@ public class Game {
 		Scanner scanner = new Scanner(System.in);
 		int classChoice = scanner.nextInt();
 		scanner.nextLine(); // consume the newline character
+		scanner.close();
 
 		switch (classChoice) {
 		case 1:
@@ -117,5 +125,6 @@ public class Game {
 		} else {
 			System.exit(0);
 		}
+		scanner.close();
 	}
 }
--- src/com/skillstorm/project1/adventure/Player.java
+++ src/com/skillstorm/project1/adventure/Player.java
@@ -1,47 +1,47 @@
 package com.skillstorm.project1.adventure;
 
 public abstract class Player {
-    private String name;
-    private int strength;
-    private int agility;
+	private String name;
+	private int strength;
+	private int agility;
 
-    public Player(String name, int strength, int agility) {
-        this.name = name;
-        this.strength = strength;
-        this.agility = agility;
-    }
+	public Player(String name, int strength, int agility) {
+		this.name = name;
+		this.strength = strength;
+		this.agility = agility;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getStrength() {
-        return strength;
-    }
+	public int getStrength() {
+		return strength;
+	}
 
-    public int getAgility() {
-        return agility;
-    }
+	public int getAgility() {
+		return agility;
+	}
 
-    public abstract String getClassName();
+	public abstract String getClassName();
 
-    public static class Warrior extends Player {
-        public Warrior(String name, int strength) {
-            super(name, strength, 0);
-        }
+	public static class Warrior extends Player {
+		public Warrior(String name, int strength) {
+			super(name, strength, 0);
+		}
 
-        public String getClassName() {
-            return "Warrior";
-        }
-    }
+		public String getClassName() {
+			return "Warrior";
+		}
+	}
 
-    public static class Rogue extends Player {
-        public Rogue(String name, int agility) {
-            super(name, 0, agility);
-        }
+	public static class Rogue extends Player {
+		public Rogue(String name, int agility) {
+			super(name, 0, agility);
+		}
 
-        public String getClassName() {
-            return "Rogue";
-        }
-    }
+		public String getClassName() {
+			return "Rogue";
+		}
+	}
 }
