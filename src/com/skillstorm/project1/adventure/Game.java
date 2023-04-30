9957b6913fb355bb45d3f5214c747c563029a5c2
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-04-30 14:21:07 -0400

    final push

--- src/com/skillstorm/project1/adventure/Game.java
+++ src/com/skillstorm/project1/adventure/Game.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Game {
 	private Player player;
 	private int currentRoom;
+	int numFightsMax = 3; // How many rounds of fight
 
 	public static void main(String[] args) {
 		Game game = new Game();
@@ -22,7 +23,7 @@ public class Game {
 
 		player = chooseClass(name);
 
-		System.out.println("Welcome to the game, " + player.getName() + " the " + player.getClassName() + "!");
+		System.out.println("Welcome to the game, where you can't loose! " + player.getName() + " the " + player.getClassName() + "!");
 		if (player.getClassName().equals("Warrior")) {
 			System.out.println("Your stats: Strength=" + player.getStrength() + " Agility=" + player.getAgility()
 					+ "\nYou are very Strong but slow as a turtle.");
@@ -34,7 +35,7 @@ public class Game {
 		System.out.print("\nPress Enter Key to Start the Game!");
 		String in1 = scanner.nextLine();
 
-		// Initialize the rooms and monsters
+// Initialize the rooms and monsters
 		String[] rooms = new String[] { "living room", "kitchen", "bathroom", "bedroom", "attic", "basement", "garage",
 				"office", "library" };
 		String[] monsters = new String[] { "zombie", "vampire", "werewolf", "witch", "ghost", "goblin", "dragon",
@@ -44,7 +45,7 @@ public class Game {
 
 		int numFights = 0; // initialize fight counter
 
-		// Start the game loop
+// Start the game loop
 		while (true) {
 			System.out.println("\nYou are currently in the " + rooms[currentRoom] + ".");
 			System.out.println("There is a " + monsters[currentRoom] + " in the room.");
@@ -53,35 +54,39 @@ public class Game {
 			System.out.println("2. Run");
 
 			int choice = scanner.nextInt();
-			scanner.nextLine(); // consume the newline character
+			scanner.nextLine(); // User decision
 
 			if (player instanceof Player.Warrior) {
 				if (choice == 1) {
-					System.out.println("You fought bravely. Doue to your immense stringth, have defeated the "
+					System.out.println("You fought bravely. Due to your immense stringth, have defeated the "
 							+ monsters[currentRoom] + "! You found the next room and entered.");
 					numFights++;
 					currentRoom++;
 				} else {
 					System.out.println("You tried to run away but you are too slow. " + monsters[currentRoom]
 							+ " cought up to you!");
-					gameOver(scanner);
-					break;
+					continue;
+//					gameOver(scanner);
+//					break;
 				}
 			} else if (player instanceof Player.Rogue) {
 				if (choice == 1) {
-					System.out.println("You bravely try to fight but having no strength " + monsters
+					System.out.println("You bravely try to fight but having no strength " + monsters[currentRoom]
 							+ " attecked you! You were Luckally able to avoide the monster!");
+//					continue;
+
 					gameOver(scanner);
-					break;
 				} else {
 					System.out.println("You have successfully run away from the " + monsters[currentRoom] + ".");
 					currentRoom++;
 					numFights++;
 				}
 			}
-			if (numFights == 3) { // check if 3 fights have been completed
+			if (numFights == numFightsMax) { // check if 3 fights have been completed
 				System.out.println(
-						"*******Congratulations, you have cleared all the monsters and reached the last room /n You have reached the exit! You are Free!*******");
+						"*******Congratulations, you have cleared all the monsters and reached the last room*******");
+				System.out.println(
+						"*******You have reached the exit! You are Free!*******");
 				gameOver(scanner);
 				break;
 			}
